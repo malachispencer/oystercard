f50798b95438715059a5c656d5cc1819cd604558
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-02 22:23:08 +0000

    Created station file and test file, first test makes user set name of station

---
+++ spec/station_spec.rb
@@ -0,0 +1,10 @@
+require 'station'
+
+describe Station do
+  describe '#initialize' do
+    it 'makes the user to name the station' do
+      station = Station.new('neasden')
+      expect(subject.name).to eq('neasden')
+    end
+  end
+end
