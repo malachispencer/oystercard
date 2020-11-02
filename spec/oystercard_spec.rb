afcefb27f65fe8ce86c8dac447ba6c704e7fdf5d
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-02 20:09:55 +0000

    Refactored top_up test and moved deduct test to bottom

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,12 +1,6 @@
 require 'oystercard'
 
 describe Oystercard do
-  context 'Class' do
-    it 'returns an instance of an Oystercard' do
-      expect(subject).to be_instance_of(Oystercard)
-    end
-  end
-  
   context '#initialize' do
     it 'sets a default balance of 0 to our card' do
       expect(subject.balance).to eq(0)
@@ -23,33 +17,18 @@ describe Oystercard do
     end
 
     it 'raises an exception when balance exceeds the maximum limit' do
-      subject.top_up(Oystercard::MAX_BALANCE)
-      expect { subject.top_up(1) }.to raise_error(Exception, 'Balance cannot be above 90')
-    end
-  end
-
-  context '#deduct' do
-    it 'deducts a fare from an Oystercard instance' do
-      subject.top_up(5)
-      expect(subject.send(:deduct)).to eq(4)
+      subject.top_up(89)
+      expect { subject.top_up(2) }.to raise_error('Balance cannot be above 90')
     end
   end
 
   context '#in_journey?' do
-    it 'can be called on an Oystercard instance' do
-      expect(subject).to respond_to(:in_journey?)
-    end
-
     it 'returns a boolean value' do
       expect(subject.in_journey?).to eq(false).or eq(true)
     end
   end
 
   context '#touch_in' do
-    it 'can be called on an Oystercard instance' do
-      expect(subject).to respond_to(:touch_in)
-    end
-
     it 'changes in_journey instance variable from false to true' do
       subject.top_up(5)
       subject.touch_in
@@ -57,15 +36,11 @@ describe Oystercard do
     end
 
     it 'raises an exception if balance is below 1' do
-      expect { subject.touch_in }.to raise_error(Exception, 'Insufficient funds')
+      expect { subject.touch_in }.to raise_error('Insufficient funds')
     end
   end
 
   context '#touch_out' do
-    it 'can be called on an Oystercard instance' do
-      expect(subject).to respond_to(:touch_out)
-    end
-
     it 'changes in_journey instance variable from true to false' do
       subject.top_up(5)
       subject.touch_in
@@ -77,6 +52,13 @@ describe Oystercard do
       subject.top_up(20)
       subject.touch_in
       expect { subject.touch_out }.to change { subject.balance }.by(-Oystercard::MINIMUM_FARE)
+    end
+  end
+
+  context '#deduct' do
+    it 'deducts a fare from an Oystercard instance' do
+      subject.top_up(20)
+      expect(subject.send(:deduct)).to eq(19)
     end
   end
 end
