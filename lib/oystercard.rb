1cd765e7f653fa743592f6220dd784643c26d9e3
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-02 15:53:22 +0000

    Green: top_up raises error if new balance would exceed limit

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,12 +2,20 @@ class Oystercard
   attr_reader :balance
 
   DEFAULT_BALANCE = 0
+  MAX_BALANCE = 90
 
   def initialize
     @balance = DEFAULT_BALANCE
   end
 
-  def top_up(money)
-    @balance += money
+  def top_up(value)
+    raise Exception.new 'Balance cannot be above 90' if exceeds_limit?
+    @balance += value
+  end
+
+  private
+
+  def exceeds_limit?
+    @balance >= MAX_BALANCE
   end
 end
