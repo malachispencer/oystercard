bba170bb4ca010c6c683ac2b48cbcf1cc6368827
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-02 23:00:47 +0000

    Green: zone is randomly assigned to Station on initialize

--- lib/station.rb
+++ lib/station.rb
@@ -1,9 +1,23 @@
 class Station
+
+  ZONES = [1, 2, 3, 4]
+
   def initialize(name)
     @name = name
+    @zone = assign_zone
   end
 
   def name
     @name
+  end
+
+  def zone
+    @zone
+  end
+
+  private
+
+  def assign_zone
+    ZONES.sample
   end
 end
--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,9 +2,14 @@ require 'station'
 
 describe Station do
   describe '#initialize' do
-    it 'makes the user to name the station' do
+    it 'makes the user name the station' do
       station = Station.new('neasden')
       expect(station.name).to eq('neasden')
+    end
+
+    it 'randomly assigns zone between 1 and 4' do
+      station = Station.new('neasden')
+      expect(1..4).to include(station.zone)
     end
   end
 end
