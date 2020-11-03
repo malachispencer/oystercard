91ad75bb323d95dcb3f7d6c4c704c76ba04bf6f5
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-03 20:17:31 +0000

    Red: Added test for Journey to initially have exit_station as nil

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,10 +2,16 @@ require 'journey'
 
 describe Journey do
   describe '#initialize' do
+  let(:entry_station) { double('entry_station', :name => :neasden, :zone => 3) }
+
     it 'takes in an entry station as a parameter' do 
-      entry_station = double('entry_station', :name => :neasden, :zone => 3)
       journey = Journey.new(entry_station)
       expect(journey.entry_station.name).to eq(:neasden)
+    end
+
+    it 'sets exit station to nil initially' do
+      journey = Journey.new(entry_station)
+      expect(journey.exit_station).to be_nil
     end
   end
 end
