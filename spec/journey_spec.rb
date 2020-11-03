6845823f6883f76450996f96800a20d573c64cf5
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-03 20:52:31 +0000

    Red: Added test for add_exit_station method

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,7 +2,7 @@ require 'journey'
 
 describe Journey do
   describe '#initialize' do
-  let(:entry_station) { double('entry_station', :name => :neasden, :zone => 3) }
+    let(:entry_station) { double('entry_station', :name => :neasden, :zone => 3) }
 
     it 'takes in an entry station as a parameter' do 
       journey = Journey.new(entry_station)
@@ -12,6 +12,17 @@ describe Journey do
     it 'sets exit station to nil initially' do
       journey = Journey.new(entry_station)
       expect(journey.exit_station).to be_nil
+    end
+  end
+
+  describe '#add_exit_station' do
+    let(:entry_station) { double('entry_station', :name => :neasden, :zone => 3) }
+    let(:exit_station) { double('exit_station', :name => :westminster, :zone => 1) }
+
+    it 'adds exit station to Journey' do
+      journey = Journey.new(entry_station)
+      journey.add_exit_station(exit_station)
+      expect(journey.exit_station).to eq(:westminster)
     end
   end
 end
