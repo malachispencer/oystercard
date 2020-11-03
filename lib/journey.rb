bf06f9b8f32733406da2ab67bd608fd10630236f
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-03 20:19:30 +0000

    Green: Journey initially has exit_station as nil

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,8 @@
 class Journey
-  attr_reader :entry_station
-  
+  attr_reader :entry_station, :exit_station
+
   def initialize(entry_station)
     @entry_station = entry_station
+    @exit_station = nil
   end
 end
