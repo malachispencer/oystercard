4091f9037c2d280ff09110ff9b89c95400554603
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-03 08:57:42 +0000

    Refactored initialize setting name test

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -3,7 +3,7 @@ require 'station'
 describe Station do
   describe '#initialize' do
     it 'sets the name of a station' do
-      expect(subject.name).not_to be(nil)
+      expect(subject.name).to be_instance_of(Symbol)
     end
 
     it 'sets a valid zone, from 1 to 4' do
