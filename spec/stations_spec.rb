f03a600ce9153f7d160478db1366afec50ac7505
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-03 08:41:31 +0000

    Refactored get_station test

--- spec/stations_spec.rb
+++ spec/stations_spec.rb
@@ -2,7 +2,7 @@ require 'stations'
 
 describe Stations do
   describe '#get_station' do
-    it 'returns a Hash of a station' do
+    it 'returns a station in the form of a hash' do
       expect(subject.get_station).to be_instance_of(Hash)
     end
   end
