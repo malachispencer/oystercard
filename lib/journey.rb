ad9a715fd688c591045604b8c54cdc2cf7ea4091
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 09:22:22 +0000

    Started Journey from scratch, entry_station is nil by default

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,26 +1,7 @@
 class Journey
-  attr_reader :entry_station, :exit_station, :fare
-
-  MINIMUM_FARE = 1
-  PENALTY_FARE = 6
-
-  def initialize(entry_station)
-    @entry_station = entry_station
-    @exit_station = nil
-  end
-
-  def finalize(exit_station)
-    add_exit_station(exit_station)
-    calculate_fare
-  end
-
-  private
-
-  def add_exit_station(exit_station)
-    @exit_station = exit_station
-  end
-
-  def calculate_fare
-    @fare = MINIMUM_FARE
+  attr_reader :entry_station
+  
+  def initialize(entry_station = nil)
+    @entry_station = nil
   end
 end
