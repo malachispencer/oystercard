1f6af79e6e10534e6b0f885e38bdc8cb68ced2eb
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 09:21:08 +0000

    Starting Journey tests from scratch, added test for entry_station to be nil by default

--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,33 +2,8 @@ require 'journey'
 
 describe Journey do
   describe '#initialize' do
-    let(:entry_station) { double('entry_station', :name => :neasden, :zone => 3) }
-
-    it 'takes in an entry station as a parameter' do 
-      journey = Journey.new(entry_station)
-      expect(journey.entry_station.name).to eq(:neasden)
-    end
-
-    it 'sets exit station to nil initially' do
-      journey = Journey.new(entry_station)
-      expect(journey.exit_station).to be_nil
-    end
-  end
-
-  describe '#finalize' do
-    let(:entry_station) { double('entry_station', :name => :neasden, :zone => 3) }
-    let(:exit_station) { double('exit_station', :name => :westminster, :zone => 1) }
-
-    it 'adds exit station to Journey' do
-      journey = Journey.new(entry_station)
-      journey.finalize(exit_station)
-      expect(journey.exit_station.name).to eq(:westminster)
-    end
-
-    it 'calculates the fare of a complete Journey' do
-      journey = Journey.new(entry_station)
-      journey.finalize(exit_station)
-      expect(journey.fare).to eq(1)
+    it 'by default sets entry_station to nil' do
+      expect(subject.entry_station).to be_nil
     end
   end
 end
