e74f44a54665d29f333060e14872dd13bf10d628
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 08:05:53 +0000

    Green: finalize calculates fare of complete Journey

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,5 +1,8 @@
 class Journey
-  attr_reader :entry_station, :exit_station
+  attr_reader :entry_station, :exit_station, :fare
+
+  MINIMUM_FARE = 1
+  PENALTY_FARE = 6
 
   def initialize(entry_station)
     @entry_station = entry_station
@@ -8,11 +11,16 @@ class Journey
 
   def finalize(exit_station)
     add_exit_station(exit_station)
+    calculate_fare
   end
 
   private
 
   def add_exit_station(exit_station)
     @exit_station = exit_station
+  end
+
+  def calculate_fare
+    @fare = MINIMUM_FARE
   end
 end
