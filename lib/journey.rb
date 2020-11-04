aa4290aeddf88e2dac465b10d3d89f8e8c206f5c
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-04 09:29:15 +0000

    Green: Journey allows a Station to be passed in on initialize

--- lib/journey.rb
+++ lib/journey.rb
@@ -1,7 +1,7 @@
 class Journey
   attr_reader :entry_station
-  
+
   def initialize(entry_station = nil)
-    @entry_station = nil
+    @entry_station = entry_station
   end
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -2,8 +2,15 @@ require 'journey'
 
 describe Journey do
   describe '#initialize' do
+    let(:station) { double :station }
+
     it 'by default sets entry_station to nil' do
       expect(subject.entry_station).to be_nil
+    end
+
+    it 'allows a station to be passed in' do
+      journey = Journey.new(station)
+      expect(journey.entry_station).to eq(station)
     end
   end
 end
