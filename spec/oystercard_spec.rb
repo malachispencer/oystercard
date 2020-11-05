0c5c91d041bf080b72715e702780c31f8f49fc54
Malachi Spencer <malachispencer@Malachis-Air.fritz.box>
2020-11-05 21:34:14 +0000

    Minor refactors

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -5,14 +5,10 @@ describe Oystercard do
     it 'sets a default balance of 0 to our card' do
       expect(subject.balance).to eq(0)
     end
-
-    it 'has an empty list of journeys by default' do
-      expect(subject.journeys).to be_empty
-    end
   end
 
   describe '#top_up' do
-    it "can be called on an Oystcard instance" do
+    it "can be called on an Oystercard instance" do
       expect(subject).to respond_to(:top_up).with(1).argument
     end
 
@@ -27,46 +23,22 @@ describe Oystercard do
   end
 
   describe '#touch_in' do
-    let(:station) { double :station }
-
-    it 'raises an exception if balance is below 1' do
-      expect { subject.touch_in(station) }.to raise_error('Insufficient funds')
-    end
-
-    it 'changes @current_journey from nil to Journey' do
-      subject.top_up(1)
-      expect { subject.touch_in(station) }.to change { subject.journey }
+    let(:journey) { double :journey }
+    it 'raises an exception if balance is below minimum fare' do
+      expect { subject.touch_in(journey) }.to raise_error('Insufficient funds')
     end
   end
 
   describe '#touch_out' do
-    let(:entry_station) { double :entry_station }
-    let(:exit_station) { double :exit_station }
-
-    it 'charges the Oystercard by minimum fare' do
-      subject.top_up(1)
-      subject.touch_in(entry_station)
-      expect { subject.touch_out(exit_station) }.to change { subject.balance }.by(-Oystercard::MINIMUM_FARE)
-    end
-
-    it 'adds a journey to journeys' do
-      subject.top_up(1)
-      subject.touch_in(entry_station)
-      expect { subject.touch_out(exit_station) }.to change { subject.journeys.length }.by(1)
-    end
-
-    it 'changes @journey from Journey to nil' do
-      subject.top_up(1)
-      subject.touch_in(entry_station)
-      subject.touch_out(exit_station)
-      expect(subject.journey).to be_nil
+    it 'can be called on a card with 1 argument' do
+      expect(subject).to respond_to(:touch_out).with(1).argument
     end
   end
 
-  describe '#deduct' do
-    it 'deducts a fare from an Oystercard instance' do
-      subject.top_up(20)
-      expect(subject.send(:deduct_fare)).to eq(19)
+  describe '#deduct_fare' do
+    it 'reduces balance by amount/fare passed into it' do
+      subject.top_up(6)
+      expect { subject.deduct_fare(6) }.to change { subject.balance }.by(-6)
     end
   end
 end
