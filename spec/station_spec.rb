81a0e3ebf58f6ac7ab2caba5f1e0e66501a2344b
Malachi Spencer <malachispencer@MA309.fritz.box>
2020-11-06 11:08:56 +0000

    Minor refactor to initialize tests

--- spec/station_spec.rb
+++ spec/station_spec.rb
@@ -2,11 +2,11 @@ require 'station'
 
 describe Station do
   describe '#initialize' do
-    it 'sets the name of a station' do
-      expect(subject.name).to be_instance_of(Symbol)
+    it 'generates a station name by default' do
+      expect(subject.name).to_not be_nil
     end
 
-    it 'sets a valid zone, from 1 to 4' do
+    it 'sets a zone from 1 to 4 by default' do
       expect(1..4).to include(subject.zone)
     end
   end
